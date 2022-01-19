f70d4baa3f7e889e9f21263746b531c883aba592
Daniel Carlson Hörnlund <info@danielcarlson.net>
2022-01-19 20:05:56 +0000

    Rename test add icon to android apk

--- UI/Mobile/Mobile.Android/MainActivity.cs
+++ UI/Mobile/Mobile.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.Views;
 
 namespace MedGame.UI.Mobile.Droid
 {
-    [Activity(Label = "MedGame.UI.Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "MediGotchi", Icon = "@mipmap/buddaicon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
