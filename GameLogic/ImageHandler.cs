f505010990a0d987de58339deaa05f5277172c5b
Daniel Carlson Hörnlund <info@danielcarlson.net>
2022-01-27 01:20:22 +0000

    Update algorithm for Progressbar images

--- GameLogic/ImageHandler.cs
+++ GameLogic/ImageHandler.cs
@@ -47,109 +47,123 @@ namespace MedGame.GameLogic
         {
             //Funkar för baby bara nu.
 
-            var points10Procent = (int)player.Level * 0.1;
-            var points20Procent = (int)player.Level * 0.2;
-            var points30Procent = (int)player.Level * 0.3;
-            var points40Procent = (int)player.Level * 0.4;
-            var points50Procent = (int)player.Level * 0.5;
-            var points60Procent = (int)player.Level * 0.6;
-            var points70Procent = (int)player.Level * 0.7;
-            var points80Procent = (int)player.Level * 0.8;
-            var points90Procent = (int)player.Level * 0.9;
-            var points100Procent = (int)player.Level * 1;
+
 
             var progressMeterImage = string.Empty;
 
-            if (CheckBetweenNumbers(player, 0, points10Procent)) progressMeterImage = "progressMeter0.png";
-            if (CheckBetweenNumbers(player, points10Procent, points20Procent)) progressMeterImage = "progressMeter10.png";
-            if (CheckBetweenNumbers(player, points20Procent, points30Procent)) progressMeterImage = "progressMeter20.png";
-            if (CheckBetweenNumbers(player, points30Procent, points40Procent)) progressMeterImage = "progressMeter30.png";
-            if (CheckBetweenNumbers(player, points40Procent, points50Procent)) progressMeterImage = "progressMeter40.png";
-            if (CheckBetweenNumbers(player, points50Procent, points60Procent)) progressMeterImage = "progressMeter50.png";
-            if (CheckBetweenNumbers(player, points60Procent, points70Procent)) progressMeterImage = "progressMeter60.png";
-            if (CheckBetweenNumbers(player, points70Procent, points80Procent)) progressMeterImage = "progressMeter70.png";
-            if (CheckBetweenNumbers(player, points80Procent, points90Procent)) progressMeterImage = "progressMeter80.png";
-            if (CheckBetweenNumbers(player, points90Procent, points100Procent)) progressMeterImage = "progressMeter90.png";
-            return progressMeterImage;
-        }
 
-        private static bool CheckBetweenNumbers(Player player, double number1, double number2)
-        {
             switch (player.Level)
             {
                 case Levels.Baby:
-                    if ((player.Points) >= number1 && player.Points < number2)
-                    {
-                        return true;
-                    }
+
+                    var points10Procent = ((int)player.Level * 0.1);
+                    var points20Procent = ((int)player.Level * 0.2);
+                    var points30Procent = ((int)player.Level * 0.3);
+                    var points40Procent = ((int)player.Level * 0.4);
+                    var points50Procent = ((int)player.Level * 0.5);
+                    var points60Procent = ((int)player.Level * 0.6);
+                    var points70Procent = ((int)player.Level * 0.7);
+                    var points80Procent = ((int)player.Level * 0.8);
+                    var points90Procent = ((int)player.Level * 0.9);
+                    var points100Procent = ((int)player.Level * 1);
+
+                    var tempPoints = player.Points;
+                    if (tempPoints <= points10Procent) progressMeterImage = "progressMeter0.png";
+                    else if (tempPoints > points10Procent && tempPoints <= points20Procent) progressMeterImage = "progressmeter10.png";
+                    else if (tempPoints > points20Procent && tempPoints <= points30Procent) progressMeterImage = "progressMeter20.png";
+                    else if (tempPoints > points30Procent && tempPoints <= points40Procent) progressMeterImage = "progressMeter30.png";
+                    else if (tempPoints > points40Procent && tempPoints <= points50Procent) progressMeterImage = "progressMeter40.png";
+                    else if (tempPoints > points50Procent && tempPoints <= points60Procent) progressMeterImage = "progressMeter50.png";
+                    else if (tempPoints > points60Procent && tempPoints <= points70Procent) progressMeterImage = "progressMeter60.png";
+                    else if (tempPoints > points70Procent && tempPoints <= points80Procent) progressMeterImage = "progressMeter70.png";
+                    else if (tempPoints > points80Procent && tempPoints <= points90Procent) progressMeterImage = "progressMeter80.png";
+                    else if (tempPoints > points90Procent && tempPoints < points100Procent) progressMeterImage = "progressMeter90.png";
+                    else progressMeterImage = "progressMeter100.png";
+
                     break;
                 case Levels.Child:
-                    if ((player.Points + (int)Levels.Baby) >= number1 && player.Points < number2)
+
+                    points10Procent = ((int)player.Level * 0.1) + (int)Levels.Baby;
+                    points20Procent = ((int)player.Level * 0.2) + (int)Levels.Baby;
+                    points30Procent = ((int)player.Level * 0.3) + (int)Levels.Baby;
+                    points40Procent = ((int)player.Level * 0.4) + (int)Levels.Baby;
+                    points50Procent = ((int)player.Level * 0.5) + (int)Levels.Baby;
+                    points60Procent = ((int)player.Level * 0.6) + (int)Levels.Baby;
+                    points70Procent = ((int)player.Level * 0.7) + (int)Levels.Baby;
+                    points80Procent = ((int)player.Level * 0.8) + (int)Levels.Baby;
+                    points90Procent = ((int)player.Level * 0.9) + (int)Levels.Baby;
+                    points100Procent = ((int)player.Level * 1) + (int)Levels.Baby;
+
+                    tempPoints = player.Points;
+                    if (tempPoints < points10Procent)
                     {
-                        return true;
+                        progressMeterImage = "progressMeter0.png";
+                        return progressMeterImage;
                     }
+
+                    tempPoints = player.Points + (int)Levels.Baby;
+
+                    if (tempPoints >= points10Procent && tempPoints < points20Procent) progressMeterImage = "progressmeter10.png";
+                    else if (tempPoints >= points20Procent && tempPoints < points30Procent) progressMeterImage = "progressMeter20.png";
+                    else if (tempPoints >= points30Procent && tempPoints < points40Procent) progressMeterImage = "progressMeter30.png";
+                    else if (tempPoints >= points40Procent && tempPoints < points50Procent) progressMeterImage = "progressMeter40.png";
+                    else if (tempPoints >= points50Procent && tempPoints < points60Procent) progressMeterImage = "progressMeter50.png";
+                    else if (tempPoints >= points60Procent && tempPoints < points70Procent) progressMeterImage = "progressMeter60.png";
+                    else if (tempPoints >= points70Procent && tempPoints < points80Procent) progressMeterImage = "progressMeter70.png";
+                    else if (tempPoints >= points80Procent && tempPoints < points90Procent) progressMeterImage = "progressMeter80.png";
+                    else if (tempPoints >= points90Procent && tempPoints < points100Procent) progressMeterImage = "progressMeter90.png";
+                    else progressMeterImage = "progressMeter100.png";
+
                     break;
+
                 case Levels.Teenager:
-                    if ((player.Points + (int)Levels.Child) >= number1 && player.Points < number2)
+
+                    points10Procent = ((int)player.Level * 0.1) + (int)Levels.Child;
+                    points20Procent = ((int)player.Level * 0.2) + (int)Levels.Child;
+                    points30Procent = ((int)player.Level * 0.3) + (int)Levels.Child;
+                    points40Procent = ((int)player.Level * 0.4) + (int)Levels.Child;
+                    points50Procent = ((int)player.Level * 0.5) + (int)Levels.Child;
+                    points60Procent = ((int)player.Level * 0.6) + (int)Levels.Child;
+                    points70Procent = ((int)player.Level * 0.7) + (int)Levels.Child;
+                    points80Procent = ((int)player.Level * 0.8) + (int)Levels.Child;
+                    points90Procent = ((int)player.Level * 0.9) + (int)Levels.Child;
+                    points100Procent = ((int)player.Level * 1) + (int)Levels.Child;
+
+                    tempPoints = player.Points;
+                    if (tempPoints < points10Procent)
                     {
-                        return true;
+                        progressMeterImage = "progressMeter0.png";
+                        return progressMeterImage;
                     }
+
+                    tempPoints = player.Points + (int)Levels.Child;
+                    if (tempPoints >= points10Procent && tempPoints < points20Procent) progressMeterImage = "progressmeter10.png";
+                    else if (tempPoints >= points20Procent && tempPoints < points30Procent) progressMeterImage = "progressMeter20.png";
+                    else if (tempPoints >= points30Procent && tempPoints < points40Procent) progressMeterImage = "progressMeter30.png";
+                    else if (tempPoints >= points40Procent && tempPoints < points50Procent) progressMeterImage = "progressMeter40.png";
+                    else if (tempPoints >= points50Procent && tempPoints < points60Procent) progressMeterImage = "progressMeter50.png";
+                    else if (tempPoints >= points60Procent && tempPoints < points70Procent) progressMeterImage = "progressMeter60.png";
+                    else if (tempPoints >= points70Procent && tempPoints < points80Procent) progressMeterImage = "progressMeter70.png";
+                    else if (tempPoints >= points80Procent && tempPoints < points90Procent) progressMeterImage = "progressMeter80.png";
+                    else if (tempPoints >= points90Procent && tempPoints < points100Procent) progressMeterImage = "progressMeter90.png";
+                    else progressMeterImage = "progressMeter100.png";
+
                     break;
-                case Levels.Pupil:
-                    if ((player.Points + (int)Levels.Teenager) >= number1 && player.Points < number2)
-                    {
-                        return true;
-                    }
-                    break;
-                case Levels.YoungAdult:
-                    if ((player.Points + (int)Levels.Pupil) >= number1 && player.Points < number2)
-                    {
-                        return true;
-                    }
-                    break;
-                case Levels.Adult:
-                    if ((player.Points + (int)Levels.YoungAdult) >= number1 && player.Points < number2)
-                    {
-                        return true;
-                    }
-                    break;
-                case Levels.OldAdult:
-                    if ((player.Points + (int)Levels.Adult) >= number1 && player.Points < number2)
-                    {
-                        return true;
-                    }
-                    break;
-                case Levels.Old:
-                    if ((player.Points + (int)Levels.OldAdult) >= number1 && player.Points < number2)
-                    {
-                        return true;
-                    }
-                    break;
-                case Levels.Master:
-                    if ((player.Points + (int)Levels.Old) >= number1 && player.Points < number2)
-                    {
-                        return true;
-                    }
-                    break;
-                case Levels.Munk:
-                    if ((player.Points + (int)Levels.Master) >= number1 && player.Points < number2)
-                    {
-                        return true;
-                    }
-                    break;
-                case Levels.God:
-                    if ((player.Points + (int)Levels.Munk) >= number1 && player.Points < number2)
-                    {
-                        return true;
-                    }
-                    break;
+
                 default:
                     break;
+
             }
 
 
 
-            return false;
+
+            return progressMeterImage;
+        }
+
+        private void CheckBetweenNumbers(double number1, double number2)
+        {
+            //player.Points < points100Procent && player.Points >= points90Procent
         }
     }
 }
