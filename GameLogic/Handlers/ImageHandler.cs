1f7f2b8fe4fc21b5b9fe10605feeb91c3e8734d9
Daniel Carlson Hörnlund <daniel.carlson.hornlund@kpmg.se>
2023-06-26 14:59:36 +0200

    Start adding unittest for progressbar

--- GameLogic/Handlers/ImageHandler.cs
+++ GameLogic/Handlers/ImageHandler.cs
@@ -13,30 +13,28 @@ namespace MedGame.GameLogic
                 tamagotchiImage = player.Level + "_Sick.png";
             }
 
-            if (player.Health < 48 && player.Health >= 24)
+            else if (player.Health < 48 && player.Health >= 24)
             {
                 tamagotchiImage = player.Level + "_VerySad.png";
             }
 
-            if (player.Health < 72 && player.Health >= 48)
+            else if (player.Health < 72 && player.Health >= 48)
             {
                 tamagotchiImage = player.Level + "_Crying.png";
             }
 
-            if (player.Health < 96 && player.Health >= 72)
+            else if (player.Health < 96 && player.Health >= 72)
             {
                 tamagotchiImage = player.Level + "_Sad.png";
             }
 
-            if (player.Health < 120 && player.Health >= 96)
+            else if (player.Health < 120 && player.Health >= 96)
             {
                 tamagotchiImage = player.Level + "_Annoyed.png";
             }
 
-            if (player.Health >= 120)
-            {
+            else
                 tamagotchiImage = player.Level + "_Zen.png";
-            }
 
             return tamagotchiImage;
         }
@@ -82,7 +80,7 @@ namespace MedGame.GameLogic
 
         public static string GetProgressBarImage(Player player)
         {
-            string progressMeterImage;
+            string progressMeterImage = string.Empty;
 
             var points0Procent = (int)player.Level * 1;
             var points10Procent = (int)player.Level * 1.1;
@@ -94,18 +92,21 @@ namespace MedGame.GameLogic
             var points70Procent = (int)player.Level * 1.7;
             var points80Procent = (int)player.Level * 1.8;
             var points90Procent = (int)player.Level * 1.9;
+            var points100Procent = (int)player.Level * 2;
 
+            var pointsInkLevelPoints = player.Points + (int)player.Level;
 
-            if (player.Points > points0Procent && player.Points <= points10Procent) progressMeterImage = "progressMeter0.png";
-            else if (player.Points > points10Procent && player.Points <= points20Procent) progressMeterImage = "progressMeter10.png";
-            else if (player.Points > points20Procent && player.Points <= points30Procent) progressMeterImage = "progressMeter20.png";
-            else if (player.Points > points30Procent && player.Points <= points40Procent) progressMeterImage = "progressMeter30.png";
-            else if (player.Points > points40Procent && player.Points <= points50Procent) progressMeterImage = "progressMeter40.png";
-            else if (player.Points > points50Procent && player.Points <= points60Procent) progressMeterImage = "progressMeter50.png";
-            else if (player.Points > points60Procent && player.Points <= points70Procent) progressMeterImage = "progressMeter60.png";
-            else if (player.Points > points70Procent && player.Points <= points80Procent) progressMeterImage = "progressMeter70.png";
-            else if (player.Points > points80Procent && player.Points <= points90Procent) progressMeterImage = "progressMeter80.png";
-            else progressMeterImage = "progressMeter100.png";
+            if (pointsInkLevelPoints > points0Procent && pointsInkLevelPoints < points10Procent) progressMeterImage = "progressMeter0.png";
+            else if (pointsInkLevelPoints >= points10Procent && pointsInkLevelPoints < points20Procent) progressMeterImage = "progressMeter10.png";
+            else if (pointsInkLevelPoints >= points20Procent && pointsInkLevelPoints < points30Procent) progressMeterImage = "progressMeter20.png";
+            else if (pointsInkLevelPoints >= points30Procent && pointsInkLevelPoints < points40Procent) progressMeterImage = "progressMeter30.png";
+            else if (pointsInkLevelPoints >= points40Procent && pointsInkLevelPoints < points50Procent) progressMeterImage = "progressMeter40.png";
+            else if (pointsInkLevelPoints >= points50Procent && pointsInkLevelPoints < points60Procent) progressMeterImage = "progressMeter50.png";
+            else if (pointsInkLevelPoints >= points60Procent && pointsInkLevelPoints < points70Procent) progressMeterImage = "progressMeter60.png";
+            else if (pointsInkLevelPoints >= points70Procent && pointsInkLevelPoints < points80Procent) progressMeterImage = "progressMeter70.png";
+            else if (pointsInkLevelPoints >= points80Procent && pointsInkLevelPoints < points90Procent) progressMeterImage = "progressMeter80.png";
+            else if (pointsInkLevelPoints >= points90Procent && pointsInkLevelPoints < points100Procent) progressMeterImage = "progressMeter90.png";
+            else if (pointsInkLevelPoints >= points10Procent) progressMeterImage = "progressMeter100.png";
 
             return progressMeterImage;
         }
---
+++ MedGame.UnitTests/HandlersTests/ImageHandlerTests.cs
@@ -0,0 +1,78 @@
+﻿using MedGame.GameLogic;
+using MedGame.Models;
+using System.Reflection.Emit;
+using Xunit;
+
+namespace MedGame.UnitTests
+{
+    public class ImageHandlerTests
+    {
+        [Theory]
+        [InlineData(23, Levels.Baby, "Sick.png")]
+        [InlineData(47, Levels.Baby, "VerySad.png")]
+        [InlineData(71, Levels.Baby, "Crying.png")]
+        [InlineData(95, Levels.Baby, "Sad.png")]
+        [InlineData(119, Levels.Baby, "Annoyed.png")]
+        [InlineData(121, Levels.Baby, "Zen.png")]
+        [InlineData(23, Levels.Child, "Sick.png")]
+        [InlineData(47, Levels.Child, "VerySad.png")]
+        [InlineData(71, Levels.Child, "Crying.png")]
+        [InlineData(95, Levels.Child, "Sad.png")]
+        [InlineData(119, Levels.Child, "Annoyed.png")]
+        [InlineData(121, Levels.Child, "Zen.png")]
+        [InlineData(23, Levels.Teenager, "Sick.png")]
+        [InlineData(47, Levels.Teenager,"VerySad.png")]
+        [InlineData(71, Levels.Teenager,"Crying.png")]
+        [InlineData(95, Levels.Teenager,"Sad.png")]
+        [InlineData(119, Levels.Teenager, "Annoyed.png")]
+        [InlineData(121, Levels.Teenager, "Zen.png")]
+        [InlineData(23, Levels.Adult, "Sick.png")]
+        [InlineData(47, Levels.Adult,"VerySad.png")]
+        [InlineData(71, Levels.Adult,"Crying.png")]
+        [InlineData(95, Levels.Adult,"Sad.png")]
+        [InlineData(119, Levels.Adult, "Annoyed.png")]
+        [InlineData(121, Levels.Adult, "Zen.png")]
+        public void GetTamagotchiImage_Dependent_on_health_and_level_Baby(int health, Levels level, string imageName)
+        {
+            Player player = new() { Health = health, Level = level };
+            var result = ImageHandler.GetTamagotchiImage(player);
+            var fullImageName = level + "_" + imageName;
+            Assert.Equal(fullImageName, result);
+        }
+
+        [Theory]
+        [InlineData(23, "healthmeterdead.png")]
+        [InlineData(47, "healthmeterterminallyill.png")]
+        [InlineData(71, "healthmetersick.png")]
+        [InlineData(95, "healthmeterangry.png")]
+        [InlineData(119, "healthmeterirritated.png")]
+        [InlineData(121, "healthmeterzen.png")]
+        public void GetHealthMeter(int health, string imageName)
+        {
+            Player player = new() { Health = health };
+            var result = ImageHandler.GetHealthMeter(player);
+            Assert.Equal(imageName, result);
+        }
+
+        [Theory]
+        [InlineData(1, Levels.Baby, "progressMeter0.png")]
+        [InlineData(28, Levels.Baby, "progressMeter10.png")]
+        [InlineData(56, Levels.Baby, "progressMeter20.png")]
+        [InlineData(84, Levels.Baby, "progressMeter30.png")]
+        [InlineData(112, Levels.Baby, "progressMeter40.png")]
+        [InlineData(140, Levels.Baby, "progressMeter50.png")]
+        [InlineData(168, Levels.Baby, "progressMeter60.png")]
+        [InlineData(196, Levels.Baby, "progressMeter70.png")]
+        [InlineData(224, Levels.Baby, "progressMeter80.png")]
+        [InlineData(252, Levels.Baby, "progressMeter90.png")]
+        [InlineData(280, Levels.Baby, "progressMeter100.png")]
+
+        public void GetProgressBarImage(int points, Levels level, string imageName)
+        {
+            Player player = new() { Points = points , Level = level };
+            var result = ImageHandler.GetProgressBarImage(player);
+
+            Assert.Equal(imageName, result);
+        }
+    }
+}
