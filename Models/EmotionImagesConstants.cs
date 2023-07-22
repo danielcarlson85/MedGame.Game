175b60d288192ba8f3cd7683a05190446f85eee3
Daniel Carlson Hörnlund <info@danielcarlson.net>
2023-07-22 16:41:40 +0200

    removed unused images

--- Models/EmotionImagesConstants.cs
+++ Models/EmotionImagesConstants.cs
@@ -8,9 +8,9 @@ namespace MedGame.Models
     {
         public const string Zen = "_Zen.png";
         public const string Irritated = "_Irritated.png";
+        public const string Angry = "_Angry.png";
         public const string Sad = "_Sad.png";
         public const string VerySad = "_VerySad.png";
-        public const string Angry = "_Angry.png";
         public const string Annoyed = "_Annoyed.png";
     }
 }
